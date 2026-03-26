7ac9f8b47822b8ec28d37dbf6f7fbd952f16f473
Benjamin Young <bryoung1@lakeheadu.ca>
2026-03-26 14:07:04 -0400

    Make it so email setup pulls from .env instead of the os environment

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 
 from flask import app
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,12 +9,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'database.db')
     
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
     ADMINS = ['resolveticketing@gmail.com']
 
     if not MAIL_SERVER or not MAIL_USERNAME or not MAIL_PASSWORD:
